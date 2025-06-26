7e568196252cacab8e1eea62a5b9ebd58c2b16c0
Jessin-Sunny <jessinsunny04@gmail.com>
2025-06-26 22:12:54 +0530

    Customer Sign Up added

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -1,15 +1,39 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const createToken = require('../utils/generateToken');
+
 //sign-up
 const signup = async(req, res, next) => {
     try {
-        //db connection logic
-        console.log("Registered Successfully")
-        res.send("Registered Successfully")
+        const { name, email, password, profilePic } = req.body || {}
+        console.log(name, email, password)
+
+        //validating input
+        if(!name || !email || !password) {
+            return res.status(400).json({ error: "All fields are mandatory" })
+        }
+
+        //checking whether user already exists or not
+        const userExists = await User.findOne({email})
+        if(userExists) {
+            return res.status(400).json({ message: "Already Existing User "})
+        }
+
+        //hash password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        //new User
+        const newUser = new User ({name, email, password: hashedPassword, profilePic})
+        const savedUser = await newUser.save()
+        const userData = savedUser.toObject();  //Convert Mongoose document to plain JS object
+        delete userData.password    //not returning password [security]
+        return res.status(201).json({ message: "Account Created Successfully", userData })
+
     } catch (error) {
-        console.log(error)
-        res.send(error)
+        console.log(error);
+        res.status(error.status || 500).json( {error: error.message || "Internal Server Error"})
     }
 }
-
-//signin
 
 module.exports = {signup} 
--- server/models/user.js
+++ server/models/user.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true, match: /^\S+@\S+\.\S+$/ },
     phoneno: { type: String, required: true },
+    profilePic: { type: String},
     password: { type: String, required: true },
     role: {
         type: String,
