30857ee990e4797a443621282b82094dac3eb8a4
Jessin-Sunny <jessinsunny04@gmail.com>
2025-06-27 18:02:54 +0530

    Login added for admin

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const router = express.Router()
 const customerRouter = require('./customerRoute')
 const sellerRouter = require('./sellerRoute')
+const adminRouter = require('./adminRoute')
 
 //directing to customer
 router.use('/customer', customerRouter)
 //directing to seller
 router.use('/seller', sellerRouter)
+//directing to admin
+router.use('/admin', adminRouter)
 
 module.exports = router
